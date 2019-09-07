e406e155eb3c7c275c2dacdfe2ef4ed382f2991d
tongli <tonglihappy@gmail.com>
2019-09-07 18:54:52 +0800

    update

--- main.py
+++ main.py
@@ -1,28 +1,46 @@
 import tushare as ts
 from datetime import datetime as dt
+from dateutil.relativedelta import relativedelta
+import datetime
 
 def getql():
     today = dt.now()
     quarter = today.month
     if quarter >= 1 and quarter <= 3 :
-        return dt(today.year,3,31).strftime('%Y%m%d')
+        return quarter, dt(today.year,3,31).strftime('%Y%m%d')
     elif quarter >= 4 and quarter <= 6:
-        return dt(today.year,6,30).strftime('%Y%m%d')
+        return quarter, dt(today.year,6,30).strftime('%Y%m%d')
     elif quarter >=7 and quarter <= 9:
-        return dt(today.year,9,30).strftime('%Y%m%d')
+        return quarter, dt(today.year,9,30).strftime('%Y%m%d')
     else:
-        return dt(today.year,12,31).strftime('%Y%m%d')
+        return quarter, dt(today.year,12,31).strftime('%Y%m%d')
+
+def time_delta(quarter, period):
+    periods = []
+    sdate = datetime.datetime.strptime(period,'%Y%m%d')
+    edate1 = sdate - relativedelta(years=1)
+    edate2 = sdate - relativedelta(years=2)
+    edate3 = sdate - relativedelta(years=3)
+    last_year = edate1.strftime("%Y")
+
+    periods.append(edate1.strftime("%Y") + "1231")
+    periods.append(edate2.strftime("%Y") + "1231")
+    periods.append(edate3.strftime("%Y") + "1231")
+
+    return periods
 
 if __name__ == "__main__":
     ts.set_token('c21631bece923458e470682569cc5998e124a09871666de852cdd439')
     pro = ts.pro_api()
 
-    today = getql() 
+    quarter, period = getql() 
 
-    print(today)
+    #print(quarter, type(period))
+    time_delta(quarter, period)
 
-    df = pro.cashflow(ts_code='300104.SZ', start_date='20160101', end_date='20180830')
+    df = pro.cashflow(ts_code='300104.SZ', start_date='20160101', period='20190831', end_date='20190906')
 
+    print(df)
 #    print(df.end_date)
 #    print(df['c_fr_sale_sg'])
 
